fc9875377a5f850104dcdccf01b7690d7748d90a
ganguliprabodh <110383763+ganguliprabodh@users.noreply.github.com>
2022-08-01 14:22:30 +0530

    Add files via upload

---
+++ BUfferedOutputStreamExample.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.io.FileOutputStream;
+
+public class BUfferedOutputStreamExample {
+
+	public static void main(String[] args)throws Exception {
+		// TODO Auto-generated method stub
+FileOutputStream fout=new FileOutputStream("E:\\testout.txt");
+BufferedOutputStream bout=new BufferedOutputStream(fout);
+String s = "java is robust and platform independence";
+byte b[]=s.getBytes();
+bout.write(b);
+bout.flush();
+bout.close();
+fout.close();
+System.out.println("success");
+	}
+
+}
